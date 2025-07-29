ae2ea0b58f1117d7945e3b14da245d44b9719771
tonynguyen22 <69407233+tonynguyen22@users.noreply.github.com>
2025-07-29 22:16:54 +0700

    Update 1 Portfolio Tracker.py

--- 1 Portfolio Tracker.py
+++ 1 Portfolio Tracker.py
@@ -174,8 +174,7 @@ with st.expander("5. Healthcare (10%)"):
 with st.expander("6. Utilities & Renewable Energy (10%)"):
     st.markdown("""
     **Objective:** Benefit from increasing energy demand (especially from AI data centers) and the global shift towards clean energy.
-    - **Utilities:** **DUK** (Duke Energy)
-    - **Renewable Energy (Pure Play):** **FSLR** (First Solar), **ENPH** (Enphase Energy), :orange[**ICLN** (iShares Global Clean Energy ETF)]
+    - **Utilities & Renewable Energy:** **NEE** (NextEra Energy Inc)
     """)
 
 # Consumer Staples
